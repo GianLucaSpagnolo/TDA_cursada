75a8b8290604a2b808dca415226650670b015542
NestorPala <npalavecino@fi.uba.ar>
2024-09-21 13:14:08 -0300

    mejora ej2

--- tp1/ejercicio_2_test.py
+++ tp1/ejercicio_2_test.py
@@ -1,6 +1,7 @@
 from time import time
 import random
 from ejercicio_2_v2 import greedy2
+from ejercicio_2 import greedy
 
 
 MAPA_1 = [
@@ -38,23 +39,28 @@ MAPA_2 = [
 # print(greedy2(MAPA_2, n=9))
 
 
-NS = [16, 32, 64, 128, 256]
+# NS = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+NS = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
 
 for N in NS:
-    edificios = int(0.2 * N**2)
+    # edificios = int(0.2 * N**2)
+    edificios = 1 if (N < 5) else N
+
     mapa = list(set([(random.randint(0, N-1), random.randint(0, N-1)) for _ in range(edificios)]))
 
     # print(mapa)
 
-    # start_time1 = time()
-    # result1 = sorted(list(greedy(mapa, n=N)))
-    # end_time1 = time() - start_time1
+    start_time1 = time()
+    result1 = sorted(list(greedy(mapa, n=N)))
+    end_time1 = time() - start_time1
     
     start_time2 = time()
     result2 = sorted(list(greedy2(mapa, n=N)))
     end_time2 = time() - start_time2
 
-    # print(result1)
-    # print(f"Greedy 1 --> Elapsed: {end_time1} seconds.")
-    print(result2)
-    print(f"Greedy 2 --> Elapsed: {end_time2} seconds.")
+    print(len(result1))
+    print(f"Greedy 1 con N = {N} --> Elapsed: {end_time1} seconds.")
+    print(len(result2))
+    print(f"Greedy 2 con N = {N} --> Elapsed: {end_time2} seconds.")
+
+    print()
