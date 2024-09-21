38557a5a33050f3683a5c4754f344c66529b5e5f
NestorPala <npalavecino@fi.uba.ar>
2024-09-21 01:41:32 -0300

    mejorado ej2

--- tp1/ejercicio_2_test.py
+++ tp1/ejercicio_2_test.py
@@ -1,6 +1,6 @@
 from time import time
-from tp1.ejercicio_2 import greedy
 import random
+from ejercicio_2_v2 import greedy2
 
 
 MAPA_1 = [
@@ -35,6 +35,7 @@ MAPA_2 = [
 
 # [(5, 2), (1, 6), (6, 7), (0, 0)]
 # print(greedy(MAPA_2, n=9))
+# print(greedy2(MAPA_2, n=9))
 
 
 NS = [16, 32, 64, 128, 256]
@@ -45,16 +46,15 @@ for N in NS:
 
     # print(mapa)
 
-    start_time = time()
-    result = greedy(mapa, n=N)
-    end_time = time() - start_time
+    # start_time1 = time()
+    # result1 = sorted(list(greedy(mapa, n=N)))
+    # end_time1 = time() - start_time1
+    
+    start_time2 = time()
+    result2 = sorted(list(greedy2(mapa, n=N)))
+    end_time2 = time() - start_time2
 
-    # print(len(result))
-    print(f"Elapsed: {end_time} seconds.")
-
-
-# Elapsed: 0.0010013580322265625 seconds.
-# Elapsed: 0.016596317291259766 seconds.
-# Elapsed: 0.2564847469329834 seconds.
-# Elapsed: 4.357736825942993 seconds.
-# Elapsed: 67.72359251976013 seconds.
+    # print(result1)
+    # print(f"Greedy 1 --> Elapsed: {end_time1} seconds.")
+    print(result2)
+    print(f"Greedy 2 --> Elapsed: {end_time2} seconds.")
--- tp1/ejercicio_2_v2.py
+++ tp1/ejercicio_2_v2.py
@@ -1,29 +1,32 @@
 import math
+from ordered_set import OrderedSet
 
-def calcular_cobertura(edificio, edificios, radio, n):
+
+def calcular_cobertura2(edificio, edificios, radio, n):
     cx, cy = edificio
     x1, x2 = max(cx - radio, 0), min(cx + radio, n-1)
     y1, y2 = max(cy - radio, 0), min(cy + radio, n-1)
-    return {(x, y) for x, y in edificios if x1 <= x <= x2 and y1 <= y <= y2}
+    return OrderedSet([(x, y) for x, y in edificios if x1 <= x <= x2 and y1 <= y <= y2])
 
-def greedy(edificios, n):
-    edificios = set(edificios)
-    restaurantes = set()
+
+def greedy2(edificios, n):
+    edificios = OrderedSet(edificios)
+    restaurantes = list()
     radio = math.ceil(0.2 * n)
 
-    while edificios:
-        max_cobertura = 0
-        mejor_edificio = None
-        mejor_cobertura = None
+    while len(edificios) > 0:
+        max_cobertura = -1
+        mejor_edificio: tuple = None
+        mejor_cobertura: OrderedSet = None
 
         for edificio in edificios:
-            cobertura = calcular_cobertura(edificio, edificios, radio, n)
+            cobertura = calcular_cobertura2(edificio, edificios, radio, n)
             if len(cobertura) > max_cobertura:
                 max_cobertura = len(cobertura)
                 mejor_edificio = edificio
                 mejor_cobertura = cobertura
 
-        restaurantes.add(mejor_edificio)
+        restaurantes.append(mejor_edificio)
         edificios -= mejor_cobertura
 
     return restaurantes
