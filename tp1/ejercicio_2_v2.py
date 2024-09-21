e372982d3a4c3eb1059c332261f71c8291d5476b
bcespedes <bcespedes@fi.uba.ar>
2024-09-20 23:12:17 -0300

    Update (optimizacion) ej2 v2

--- tp1/ejercicio_2_v2.py
+++ tp1/ejercicio_2_v2.py
@@ -12,43 +12,52 @@ def dentro_rango(otro_edificio, edificio_central, radio, n):
     return (x1 <= x <= x2) and (y1 <= y <= y2)
 
 
-def calcular_cobertura(edificios, edificios_visitados, edificio, radio, n) -> list[int, list[list[int, int]]]:
+def calcular_cobertura(edificios, edificio_central, radio, n) -> list[int, list[list[int, int]]]:
     cobertura = set()
 
-    for e in edificios:
-        if e in edificios_visitados:
-            continue
-        if dentro_rango(e, edificio, radio, n):
-            cobertura.add(e)
+    for edificio in edificios:
+        if dentro_rango(edificio, edificio_central, radio, n):
+            cobertura.add(edificio)
 
-    return len(cobertura), cobertura
+    return cobertura
 
 
 def greedy(edificios, n):
     
     edificios_visitados = set()
     restaurantes = set()
+    diccionario_edificios = dict()
 
     x = round(0.2 * n)
 
-    while len(edificios_visitados) < len(edificios):
-        max_cobertura = -1
+
+    for edificio in edificios:
+        edificios_en_rango = calcular_cobertura(edificios, edificio, x, n)
+        diccionario_edificios[edificio] = edificios_en_rango
+
+    while len(diccionario_edificios) > 0:
+
+        max_cobertura = 0
         edificios_cubiertos = None
         edificio_max_cobertura = None
 
-        for edificio in edificios:
-            if edificio in edificios_visitados:
-                continue
+        for edificio in diccionario_edificios:
 
-            (cobertura, edificios_dentro_de_cobertura) = calcular_cobertura(edificios, edificios_visitados, edificio, radio=x, n=n)
+            cobertura = diccionario_edificios[edificio]
 
-            if cobertura > max_cobertura:
-                max_cobertura = cobertura
-                edificios_cubiertos = edificios_dentro_de_cobertura
+            if len(cobertura) > max_cobertura:
+                max_cobertura = len(cobertura)
+                edificios_cubiertos = cobertura
                 edificio_max_cobertura = edificio
 
         edificios_visitados.update(edificios_cubiertos)
         restaurantes.add(edificio_max_cobertura)
+    
+        for e in set(diccionario_edificios.keys()):
+            if e in edificios_cubiertos:
+                del diccionario_edificios[e]
+            else:
+                diccionario_edificios[e] -= edificios_cubiertos
 
     return restaurantes
 
