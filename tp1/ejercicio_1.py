f29061df51ea1665d3ad91a15324029b98204d94
GianLucaSpagnolo <gianluuba@gmail.com>
2024-09-21 05:13:44 -0300

    ajuste ejercicio 1

--- tp1/ejercicio_1.py
+++ tp1/ejercicio_1.py
@@ -16,38 +16,37 @@ def wrapper_detectar_moneda_falsa_lineal(bolsa_de_monedas):
     return detectar_moneda_falsa_lineal(bolsa_de_monedas, 0, len(bolsa_de_monedas)-1)
 
 
-def detectar_monedas_falsa(monedas, start_index, end_index):
+def detectar_monedas_falsa_dyc(monedas, start_index, end_index):
     # Caso base: O(1)
-    if start_index == end_index:
-        return monedas[start_index]
-    
-    if end_index - start_index == 1:
-        return monedas[start_index] if monedas[start_index] < monedas[end_index] else monedas[end_index]
+    if end_index - start_index <= 1:
+        if monedas[start_index] < monedas[end_index]:
+            return monedas[start_index]
+        else:
+            return monedas[end_index]
 
-    # Caso recursivo: O(n)
+    # Caso recursivo: O(n log n)
     mid_index = start_index + (end_index - start_index) // 2
 
-    mid = monedas[start_index:mid_index+1]
-    expected_weight = len(mid) * max(monedas[0], monedas[1])
-
-    if sum(mid) < expected_weight:
-        return detectar_monedas_falsa(monedas, start_index, mid_index)
-    else:
-        return detectar_monedas_falsa(monedas, mid_index + 1, end_index)
+    moneda_izq = detectar_monedas_falsa_dyc(monedas, start_index, mid_index)
+    if moneda_izq < monedas[mid_index + 1]:
+        return moneda_izq
+    
+    moneda_der = detectar_monedas_falsa_dyc(monedas, mid_index + 1, end_index)
+    return moneda_der
 
 def wrapper_detectar_moneda_falsa_dyc(monedas):
     """
     Versión Division y Conquista:
 
     Complejidad temporal (teorema maestro):
-    T(n) = 1 T(n/2) + O(n^1)
+    T(n) = 2 T(n/2) + O(n^1)
 
-    a = 1, b = 2, c = 1
+    a = 2, b = 2, c = 1
 
-    log_b(a) = log_1(2) = 0
+    log_b(a) = log_2(2) = 1
     
-    log_b(a) < c => O(n^c) = O(n^1)
+    log_b(a) = c => O(n^c log_b(n)) = O(n^1 log_2(n))
 
-    Complejidad temporal: O(n)
+    Complejidad temporal O(n log n)
     """
-    return detectar_monedas_falsa(monedas, 0, len(monedas) - 1)
+    return detectar_monedas_falsa_dyc(monedas, 0, len(monedas) - 1)
--- tp1/ejercicio_1_test.py
+++ tp1/ejercicio_1_test.py
@@ -3,66 +3,70 @@ from random import randint
 
 import ejercicio_1 as ej1
 
-CANTIDADES_DE_MONEDAS = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 10000000]
+CANTIDADES_DE_MONEDAS = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
 MONEDA_FALSA = 9
 
 def test_moneda_falsa_random(cantidad):
     bolsa_de_monedas = [10]*(cantidad - 1)
     random_pos = randint(0, cantidad - 1)
     bolsa_de_monedas.insert(random_pos, MONEDA_FALSA)
+
     time_lineal = time()
     moneda_lineal = ej1.wrapper_detectar_moneda_falsa_lineal(bolsa_de_monedas)
     time_lineal = time() - time_lineal
-    time_rec = time()
-    moneda_rec = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
-    time_rec = time() - time_rec
+    time_dyc = time()
+    moneda_dyc = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
+    time_dyc = time() - time_dyc
 
-    assert moneda_lineal == MONEDA_FALSA and moneda_rec == MONEDA_FALSA
+    assert moneda_lineal == MONEDA_FALSA and moneda_dyc == MONEDA_FALSA
     print(f"{cantidad} monedas (random: posicion {random_pos})")
-    print(f"Moneda falsa (lineal): {moneda_lineal}. Tiempo: {time_lineal:.8f}")
-    print(f"Moneda falsa    (dyc): {moneda_rec}. Tiempo: {time_rec:.8f}\n")
+    print(f"Moneda falsa   (lineal): {moneda_lineal} - Tiempo: {time_lineal:.8f}")
+    print(f"Moneda falsa      (dyc): {moneda_dyc} - Tiempo: {time_dyc:.8f}")
 
 def test_modena_falsa_inicio(cantidad):
     bolsa_de_monedas = [MONEDA_FALSA] + [10]*(cantidad - 1)
+
     time_lineal = time()
     moneda_lineal = ej1.wrapper_detectar_moneda_falsa_lineal(bolsa_de_monedas)
     time_lineal = time() - time_lineal
-    time_rec = time()
-    moneda_rec = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
-    time_rec = time() - time_rec
+    time_dyc = time()
+    moneda_dyc = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
+    time_dyc = time() - time_dyc
 
-    assert moneda_lineal == MONEDA_FALSA and moneda_rec == MONEDA_FALSA
+    assert moneda_lineal == MONEDA_FALSA and moneda_dyc == MONEDA_FALSA
     print(f"{cantidad} monedas (caso inicio)")
-    print(f"Moneda falsa (lineal): {moneda_lineal}. Tiempo: {time_lineal:.8f}")
-    print(f"Moneda falsa    (dyc): {moneda_rec}. Tiempo: {time_rec:.8f}\n")
+    print(f"Moneda falsa   (lineal): {moneda_lineal} - Tiempo: {time_lineal:.8f}")
+    print(f"Moneda falsa      (dyc): {moneda_dyc} - Tiempo: {time_dyc:.8f}")
 
 def test_moneda_falsa_mitad(cantidad):
-    bolsa_de_monedas = [10]*(cantidad//2) + [MONEDA_FALSA] + [10]*(cantidad//2)
+    bolsa_de_monedas = [10]*(cantidad//2 - 1) + [MONEDA_FALSA] + [10]*((cantidad//2))
+
     time_lineal = time()
     moneda_lineal = ej1.wrapper_detectar_moneda_falsa_lineal(bolsa_de_monedas)
     time_lineal = time() - time_lineal
-    time_rec = time()
-    moneda_rec = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
-    time_rec = time() - time_rec
+    time_dyc = time()
+    moneda_dyc = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
+    time_dyc = time() - time_dyc
 
-    assert moneda_lineal == MONEDA_FALSA and moneda_rec == MONEDA_FALSA
+    assert moneda_lineal == MONEDA_FALSA and moneda_dyc == MONEDA_FALSA
     print(f"{cantidad} monedas (caso mitad)")
-    print(f"Moneda falsa (lineal): {moneda_lineal}. Tiempo: {time_lineal:.8f}")
-    print(f"Moneda falsa    (dyc): {moneda_rec}. Tiempo: {time_rec:.8f}\n")
+    print(f"Moneda falsa   (lineal): {moneda_lineal} - Tiempo: {time_lineal:.8f}")
+    print(f"Moneda falsa      (dyc): {moneda_dyc} - Tiempo: {time_dyc:.8f}")
 
 def test_modena_falsa_final(cantidad):
     bolsa_de_monedas = [10]*(cantidad - 1) + [MONEDA_FALSA]
+
     time_lineal = time()
     moneda_lineal = ej1.wrapper_detectar_moneda_falsa_lineal(bolsa_de_monedas)
     time_lineal = time() - time_lineal
-    time_rec = time()
-    moneda_rec = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
-    time_rec = time() - time_rec
+    time_dyc = time()
+    moneda_dyc = ej1.wrapper_detectar_moneda_falsa_dyc(bolsa_de_monedas)
+    time_dyc = time() - time_dyc
 
-    assert moneda_lineal == MONEDA_FALSA and moneda_rec == MONEDA_FALSA
+    assert moneda_lineal == MONEDA_FALSA and moneda_dyc == MONEDA_FALSA
     print(f"{cantidad} monedas (caso final)")
-    print(f"Moneda falsa (lineal): {moneda_lineal}. Tiempo: {time_lineal:.8f}")
-    print(f"Moneda falsa    (dyc): {moneda_rec}. Tiempo: {time_rec:.8f}\n")
+    print(f"Moneda falsa   (lineal): {moneda_lineal} - Tiempo: {time_lineal:.8f}")
+    print(f"Moneda falsa      (dyc): {moneda_dyc} - Tiempo: {time_dyc:.8f}")
 
 
 # Testeo de casos de prueba
