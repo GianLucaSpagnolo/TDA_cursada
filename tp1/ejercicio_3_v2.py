b699c44d50d5ac8996cc47d0aab165829647bd29
pablogch <pablochoconi@gmail.com>
2024-09-22 14:30:47 -0300

    Refactor problema 3

--- tp1/ejercicio_3_v2.py
+++ tp1/ejercicio_3_v2.py
@@ -1,65 +1,36 @@
-def compatible(magickConstant, sideSize, square, collection, rol, col):
-    if len(collection) < sideSize and sum(collection) < magickConstant:
-        return True
-    if sum(collection) == magickConstant and len(collection) == sideSize:
-        return True
+def compatible(magickConstant, square, row, col, value):
+    if sum(square[row]) + value > magickConstant:
+        return False
+    return True
+
+def fill(square, sideSize, magickConstant, possibleValues, row, col):
+    for value in possibleValues:
+        if len(square) == row:
+            square.append([])
+        if compatible(magickConstant, square, row, col, value):
+            # SE CREA COPIA DE LISTA DE VALORES POSIBLES PARA NO MODIFICAR EL ORIGINAL
+            # YA QUE SE ESTA ITERANDO Y PUEDE CREAR INCONSISTENCIAS
+            possibleValuesOfNext = possibleValues.copy()
+            square[row].append(value)
+            print(square)
+            possibleValuesOfNext.pop(0)
+            if len(square[row]) == sideSize:
+                row += 1
+                col = 0
+            else:
+                col += 1
+            result = fill(square, sideSize, magickConstant, possibleValuesOfNext, row, col)
+            if result:
+                return True
+            else:
+                square[row].pop()
     return False
-
-def restorePossibleValues(possibleValues, failedValues):
-    for i in reversed(failedValues):
-        possibleValues.append(i)
-    failedValues.clear()
-
-
-def fillRow(magickConstant, sideSize, square, possibleValues, failedValues, row, col, value):
-    # RETORNA TRUE SI LOGRA LLENAR LA FILA
-    if len(square) == row:  # APENDEA FILA SI NO EXISTE
-        square.append([])
-
-    square[row].append(value)
-
-    print(square)
-
-    if compatible(magickConstant, sideSize, square, square[row], row, col):
-        # RECUPERA VALORES FALLIDOS PARA LAS SIGUIENTES SOLUCIONES PARCIALES
-        restorePossibleValues(possibleValues, failedValues)
-
-        if len(square[row]) == sideSize:
-            return True
-
-    else:
-        # NO ES SOLUCION PARCIAL LA QUITA DE LA FILA
-        square[row].pop()
-        failedValues.append(value)
-        if len(possibleValues) == 0:
-            return False
-
-    # ANALIZA SIGUIENTES SOLUCIONES PARCIALES
-    newValue = possibleValues.pop()
-    return fillRow(magickConstant, sideSize, square, possibleValues, failedValues, row, col + 1, newValue)
 
 
 def buildMagicSquare(n):
     square = []
     possibleValues = [i for i in range(1, n*n+1)]
+    possibleValues = list(reversed(possibleValues))
     magickConstant = n * (n**2 + 1) / 2
     sideSize = n
-    row = 0
-    failedValues = []
-    
-    while len(square) != sideSize:
-        value = possibleValues.pop()
-        rowFilled = fillRow(magickConstant, sideSize, square, possibleValues, failedValues, row, 0, value)
-        if rowFilled:
-            row += 1
-        else:
-            droppedRow = list(square.pop())
-            for i in reversed(droppedRow):
-                failedValues.insert(1, i)
-            restorePossibleValues(possibleValues, failedValues)
-        pass
-
-
-
-
-
+    fill(square, sideSize, magickConstant, possibleValues, 0, 0)
