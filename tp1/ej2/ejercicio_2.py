de7f955504c9adefaf2a154c09f6a63058966edd
bcespedes <bcespedes@fi.uba.ar>
2024-09-26 20:40:14 -0300

    Update (optimización y bug fixes) ejercicio 2

--- tp1/ej2/ejercicio_2.py
+++ tp1/ej2/ejercicio_2.py
@@ -1,32 +1,60 @@
-import math
-from ordered_set import OrderedSet
+def calcular_cobertura(edificio_central: tuple, edificios: list, radio_de_cobertura: int, tamano_barrio: int) -> set:
+
+    cobertura: set = set()
+    coordenada_x_punto_central: int = edificio_central[0]
+    coordenada_y_punto_central: int = edificio_central[1]
+
+    limite_izquierdo: int = max(coordenada_x_punto_central - radio_de_cobertura, 0)
+    limite_derecho: int = min(coordenada_x_punto_central + radio_de_cobertura, tamano_barrio - 1)
+    limite_superior: int = max(coordenada_y_punto_central - radio_de_cobertura, 0)
+    limite_inferior: int = min(coordenada_y_punto_central + radio_de_cobertura, tamano_barrio - 1)
+
+    for x, y in edificios:
+        if (limite_izquierdo <= x <= limite_derecho) and (limite_superior <= y <= limite_inferior):
+            cobertura.add((x, y))
+
+    return cobertura
 
 
-def calcular_cobertura(edificio, edificios, radio, n):
-    cx, cy = edificio
-    x1, x2 = max(cx - radio, 0), min(cx + radio, n-1)
-    y1, y2 = max(cy - radio, 0), min(cy + radio, n-1)
-    return OrderedSet([(x, y) for x, y in edificios if (x1 <= x <= x2) and (y1 <= y <= y2)])
+def construir_diccionario_edificios(edificios: list, tamano_barrio: int) -> dict:
+
+    diccionario_edificios: dict = dict()
+    radio_de_cobertura: int = round(0.20 * tamano_barrio)
+    edificios.sort()
+
+    for edificio in edificios:
+        cobertura: set = calcular_cobertura(edificio, edificios, radio_de_cobertura, tamano_barrio)
+        diccionario_edificios[edificio] = cobertura
+
+    return diccionario_edificios
 
 
-def greedy(edificios, n):
-    edificios = OrderedSet(edificios)
-    restaurantes = list()
-    radio = math.ceil(0.2 * n)
+def eliminar_edificios_cubiertos(diccionario_edificios: dict, edificios_ya_cubiertos: set) -> dict:
 
-    while len(edificios) > 0:
-        max_cobertura = -1
-        mejor_edificio: tuple = None
-        mejor_cobertura: OrderedSet = None
+    for edificio_ya_visitado in edificios_ya_cubiertos:
+        del diccionario_edificios[edificio_ya_visitado]
 
-        for edificio in edificios:
-            cobertura = calcular_cobertura(edificio, edificios, radio, n)
+    for edificio in diccionario_edificios:
+        diccionario_edificios[edificio] -= edificios_ya_cubiertos
+
+
+def construccion_de_restaurantes(edificios: list, tamano_barrio: int) -> list:
+
+    restaurantes: list = list()
+    diccionario_edificios: dict = construir_diccionario_edificios(edificios, tamano_barrio)
+
+    while diccionario_edificios:
+        max_cobertura: int = 0
+        mejor_edificio: tuple = ()
+        mejor_cobertura: set = set()
+
+        for edificio, cobertura in diccionario_edificios.items():
             if len(cobertura) > max_cobertura:
                 max_cobertura = len(cobertura)
                 mejor_edificio = edificio
                 mejor_cobertura = cobertura
 
         restaurantes.append(mejor_edificio)
-        edificios -= mejor_cobertura
+        eliminar_edificios_cubiertos(diccionario_edificios, mejor_cobertura)
 
     return restaurantes
--- tp1/ej2/ejercicio_2_test.py
+++ tp1/ej2/ejercicio_2_test.py
@@ -3,32 +3,36 @@ from random import randint
 import ejercicio_2 as ej2
 
 
-N = [10, 20, 50, 100]
-CANTIDAD_EDIFICIOS = 0.2
+TAMANOS_BARRIO: list = [10, 20, 50, 100, 200, 300] # con 400 ya empieza a haber problemas... Pueden verificarlo
+CANTIDAD_EDIFICIOS: float = 0.2
 
 
-def generar_edificios_en_grilla(n):
-    cantidad_edificios = int(n * n * CANTIDAD_EDIFICIOS)
-    edificios = []
+def generar_edificios_en_grilla(tamano_barrio: int) -> list:
+
+    cantidad_edificios: int = round(tamano_barrio ** 2 * CANTIDAD_EDIFICIOS)
+    edificios: list = list()
+
     for _ in range(cantidad_edificios):
         while True:
-            edificio = (randint(0, n - 1), randint(0, n - 1))
+            edificio = (randint(0, tamano_barrio - 1), randint(0, tamano_barrio - 1))
             if edificio not in edificios:
                 edificios.append(edificio)
                 break
+
     return edificios
 
 
-def test_ej2():
-    for n in N:
-        edificios = generar_edificios_en_grilla(n)
-        #for i in range(n):
-        #    print(" ".join(["E" if (i, j) in edificios else "." for j in range(n)]))
+def test_ej2() -> None:
 
-        start_time = time()
-        restaurantes = ej2.greedy(edificios, n)
-        end_time = time() - start_time
-        print(f"Tiempo de ejecución para n={n}: {end_time} segundos\n")
+    for tamano_barrio in TAMANOS_BARRIO:
+
+        edificios: list = generar_edificios_en_grilla(tamano_barrio)
+
+        start_time: float = time()
+        restaurantes: list = ej2.construccion_de_restaurantes(edificios, tamano_barrio)
+        end_time: float = time() - start_time
+
+        print(f"Tiempo de ejecución para n = {tamano_barrio}: {end_time} segundos\n")
 
         print(f"Cantidad de restaurantes: {len(restaurantes)}\nRestaurantes: {restaurantes}\n")
 
