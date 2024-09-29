ecf457eca41eb12b483227e978c8e5919b5da1c6
bcespedes <bcespedes@fi.uba.ar>
2024-09-29 10:43:06 -0300

    Se modifica problema3_tests

--- tp1/ej3/ejercicio_3_test.py
+++ tp1/ej3/ejercicio_3_test.py
@@ -1,7 +1,27 @@
+from time import time
 from ejercicio_3 import buildMagicSquare
 
+N = [3, 4, 5]
 
-N = 3
+
+def printSquare(square, sideSize):
+    print("------" * sideSize + "-")
+    for row in square:
+        print("|", end="")
+        for cell in row:
+            str = cell.__str__()
+            if cell < 10:
+                str = " " + str
+            if cell < 100:
+                str = " " + str
+            print("", str, end=" |")
+        print("")
+    print("------" * sideSize + "-")
 
 
-buildMagicSquare(N)
+for n in N:
+    start_time = time()
+    square = buildMagicSquare(n)
+    end_time = time() - start_time
+    printSquare(square, n)
+    print(f"Tiempo de ejecución para n={n}: {end_time} segundos\n")
