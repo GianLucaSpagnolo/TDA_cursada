a0c235a93eaeb700f6abf2b1a6f9c3e43e900db3
Brian Céspedes <bcespedes@fi.uba.ar>
2024-10-28 08:41:58 -0300

    Actualizados .py ejercicio 1

---
+++ tp2/ej1/ejercicio_1_sin_lista_de_opt.py
@@ -0,0 +1,80 @@
+def elegir_trabajo_primera_semana(seleccionados: list) -> None:
+
+    opt_actual: int = max(trabajos_tranquilos[1], trabajos_estresantes[1])
+
+    if opt_actual == trabajos_tranquilos[1]:
+        seleccionados.append("t1")
+    else:
+        seleccionados.append("e1")
+
+    return opt_actual
+
+
+def elegir_trabajo_segunda_semana(seleccionados: list) -> None:
+
+    opt_anterior: int = elegir_trabajo_primera_semana(seleccionados)
+
+    opt_actual: int = max(opt_anterior + trabajos_tranquilos[2], trabajos_estresantes[2])
+
+    if opt_actual == trabajos_tranquilos[2] + opt_anterior:
+        seleccionados.append("t2")
+    else:
+        seleccionados.pop()
+        seleccionados.append("e2")
+
+    return opt_actual, opt_anterior
+
+
+def elegir_trabajo(opt_actual: int, opt_anterior: int, i: int, seleccionados: list) -> None:
+
+    opt = max(opt_actual + trabajos_tranquilos[i], opt_anterior + trabajos_estresantes[i])
+
+    if opt == trabajos_tranquilos[i] + opt_actual:
+        seleccionados.append(f"t{i}")
+    else:
+        seleccionados.pop()
+        seleccionados.append(f"e{i}")
+
+    opt_anterior = opt_actual
+    opt_actual = opt
+
+    return opt_actual, opt_anterior
+
+
+def seleccion_de_trabajos(trabajos_estresantes: list, trabajos_tranquilos: list) -> tuple:
+
+    # Agrego un 0 en el indice 0 para "descartarlo" (usaremos a partir del indice 1 para simplificar).
+    trabajos_estresantes.insert(0, 0)
+    trabajos_tranquilos.insert(0, 0)
+
+    n: int = len(trabajos_estresantes)
+
+    seleccionados: list = list()
+
+    opt_actual, opt_anterior = elegir_trabajo_segunda_semana(seleccionados)
+
+    for i in range(3, n):
+        opt_actual, opt_anterior = elegir_trabajo(opt_actual, opt_anterior, i, seleccionados)
+
+    return opt_actual, seleccionados
+
+
+print("\nEJEMPLO 1")
+trabajos_estresantes: list = [8, 6, 20, 2, 9]
+trabajos_tranquilos: list = [8, 6, 4, 5, 2]
+
+beneficio, trabajos_a_realizar = seleccion_de_trabajos(trabajos_estresantes, trabajos_tranquilos)
+print(f"\n\tSe ha obtenido un beneficio total de {beneficio}.")
+print(f"\nLos trabajos a realizar son: {trabajos_a_realizar}.")
+
+
+print("\n\n")
+
+
+print("EJEMPLO 2")
+trabajos_estresantes: list = [8, 8, 15, 7, 12, 17, 9, 29]
+trabajos_tranquilos: list = [7, 9, 5, 15, 10, 12, 11, 14]
+
+beneficio, trabajos_a_realizar = seleccion_de_trabajos(trabajos_estresantes, trabajos_tranquilos)
+print(f"\n\tSe ha obtenido un beneficio total de {beneficio}.")
+print(f"\nLos trabajos a realizar son: {trabajos_a_realizar}.\n")
