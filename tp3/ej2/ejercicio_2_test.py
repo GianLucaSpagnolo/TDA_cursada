baf8743ae420e4faf1b9d3a37a973b6f0881d0f6
NestorPala <npalavecino@fi.uba.ar>
2024-12-01 23:41:49 -0300

    cmd line execute test

--- tp3/ej2/ejercicio_2_test.py
+++ tp3/ej2/ejercicio_2_test.py
@@ -1,41 +1,49 @@
+import os
 import ejercicio_2 as ej2
-
-# Pesos de los items
-weights = [1, 7, 4, 5, 6, 8, 4, 7, 8, 4]
-# Valores de los items
-values = [15, 21, 35, 48, 53, 63, 74, 87, 99, 102]
-# Capacidad máxima de la mochila
-capacity = 25
-# Parámetro de precisión epsilon (ε)
-epsilon = 0.5
-
-# Imprimir los datos de entrada
-
-print("Datos de entrada")
-print("=====================================")
+import sys
 
 
-for i in range(len(values)):
-    print(f"Item {i + 1} - Peso: {weights[i]} - Valor: {values[i]}")
+def test_ejercicio_2(weights, values, capacity, epsilon):
+    print("Datos de entrada")
+    print("=====================================")
 
-print("\nCapacidad de la mochila:", capacity)
+    for i in range(len(values)):
+        print(f"Item {i + 1} - Peso: {weights[i]} - Valor: {values[i]}")
 
-print("\nEpsilon:", epsilon)
+    print("\nCapacidad de la mochila:", capacity)
+    print("\nEpsilon:", epsilon)
+
+    # Ejecutar la función de aproximación
+    result = ej2.knapsack_approx(weights, values, capacity, epsilon)
+
+    # Imprimir el valor óptimo aproximado
+    print("\nResultado")
+    print("=====================================")
+
+    for i in result["selectedItems"]:
+        print(f"Item {i} - Peso: {weights[i - 1]} - Valor: {values[i - 1]}")
+
+    print("Valor total:", result["totalValue"])
+    print("Peso total:", result["totalWeight"])
 
 
-# Ejecutar la función de aproximación
-result = ej2.knapsack_approx(weights, values, capacity, epsilon)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python ejercicio_2_test.py <archivo>")
+        sys.exit(1)
 
-# Imprimir el valor óptimo aproximado
-print("\nResultado")
-print("=====================================")
+    filename = sys.argv[1]
 
-for i in result["selectedItems"]:
-    print(f"Item {i} - Peso: {weights[i - 1]} - Valor: {values[i - 1]}")
+    with open(filename) as f:
+        epsilon = float(f.readline().strip())
+        capacity = int(f.readline().strip())
+        element_count = int(f.readline().strip())
+        weights = []
+        values = []
 
-print("Valor total:", result["totalValue"])
-print("Peso total:", result["totalWeight"])
+        for _ in range(element_count):
+            weight, value = map(int, f.readline().strip().split(","))
+            weights.append(weight)
+            values.append(value)
 
-
-
-
+        test_ejercicio_2(weights, values, capacity, epsilon)
